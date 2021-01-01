a373df15493038286a4a9cefd2935aacf09f992e
jrj444 <jrj444@foxmail.com>
2021-01-01 20:15:00 +0800

    完成编辑任务名字功能

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Todo(props) {
-  return (
-    <li className="todo stack-small">
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [newName, setNewName] = useState('');
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    setNewName('');
+    setIsEditing(false);
+  };
+
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input
+          id={props.id}
+          className="todo-text"
+          type="text"
+          value={newName}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setIsEditing(false)}
+        >
+          取消
+        </button>
+        <button type="submit" className="btn btn_primary todo-edit">保存</button>
+      </div>
+    </form>
+  );
+
+  const viewTemplate = (
+    <div className="stack-small">
       <div className="c-cb">
         <input
           id={props.id}
@@ -15,16 +57,27 @@ export default function Todo(props) {
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn">
-          Edit
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setIsEditing(true)}
+        >
+          编辑
         </button>
         <button
           type="button"
           className="btn btn_danger"
-          onClick={() => props.deleteTask(props.id)}>
-          Delete
+          onClick={() => props.deleteTask(props.id)}
+        >
+          删除
         </button>
       </div>
+    </div>
+  );
+
+  return (
+    <li className="todo">
+      {isEditing ? editingTemplate : viewTemplate}
     </li>
   );
 }
