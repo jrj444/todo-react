f69c73912baf7c2b91ee3ad854a2dba83e62a41b
jrj444 <jrj444@foxmail.com>
2021-01-01 14:43:24 +0800

    创建 Todo 组件

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Todo from './components/Todo';
+
 function App(props) {
   return (
     <div className="todoapp stack-large">
@@ -34,58 +36,11 @@ function App(props) {
         3 tasks remaining
       </h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
       >
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              吃饭
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit
-            </button>
-            <button type="button" className="btn btn_danger">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              睡觉
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              学习
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete
-            </button>
-          </div>
-        </li>
+        <Todo name="吃饭" completed={true} id="todo-0" />
+        <Todo name="睡觉" completed={false} id="todo-1" />
+        <Todo name="学习" completed={false} id="todo-2" />
       </ul>
     </div>
   );
